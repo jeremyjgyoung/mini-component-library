cd5e5e1d0a6df2de0f80e6724a662c1d3ffcc120
Jeremy <jeremyjgyoung@gmail.com>
2024-08-19 16:57:38 -0700

    Set up size selection of small, medium, and large with different heights

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,16 +5,30 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+const SIZES = {
+  small: {
+    "--height": "8px",
+  },
+  medium: {
+    "--height": "12px",
+  },
+  large: {
+    "--height": "16px",
+  },
+};
+
 const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+
   return (
-    <ProgressBarBase value={value} max={100}>
+    <ProgressBarBase style={styles} value={value} max={100}>
       {value}
     </ProgressBarBase>
   );
 };
 
 const ProgressBarBase = styled.progress`
-  height: 8px;
+  height: var(--height);
 `;
 
 export default ProgressBar;
