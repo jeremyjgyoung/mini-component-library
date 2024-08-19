5ee704b8e9d17152e1c0f77c168133c0ba23b91e
Jeremy <jeremyjgyoung@gmail.com>
2024-08-19 16:51:46 -0700

    Use the progress element for a progress bar and setup styling

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,20 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <ProgressBarBase value={value} max={100}>
+      {value}
+    </ProgressBarBase>
+  );
 };
+
+const ProgressBarBase = styled.progress`
+  height: 8px;
+`;
 
 export default ProgressBar;
