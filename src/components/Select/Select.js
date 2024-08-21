493dfd199d796be80c5689e4f49fdab6b31d246e
Jeremy <jeremyjgyoung@gmail.com>
2024-08-20 17:47:53 -0700

    Update Select component with color and border-radius and add chevron icon

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <div>
+      <SelectDropdown value={value} onChange={onChange}>
+        {children}
+      </SelectDropdown>
+      <Icon id="chevron-down" />
+    </div>
   );
 };
+
+const SelectDropdown = styled.select`
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+`;
 
 export default Select;
