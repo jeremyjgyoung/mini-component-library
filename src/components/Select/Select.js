cfe7f3804750c4e198a366a5e19893ec51633db3
Jeremy <jeremyjgyoung@gmail.com>
2024-08-26 16:25:09 -0700

    Change the direction of how to solve the Select component with a 0 opacity select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,18 +9,47 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <div>
+    <SelectWrapper>
+      <DisplayWrapper>
+        {displayedValue}
+        <IconWrapper>
+          <Icon id="chevron-down" />
+        </IconWrapper>
+      </DisplayWrapper>
       <SelectDropdown value={value} onChange={onChange}>
         {children}
       </SelectDropdown>
-      <Icon id="chevron-down" />
-    </div>
+    </SelectWrapper>
   );
 };
 
+const SelectWrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
 const SelectDropdown = styled.select`
-  background-color: ${COLORS.transparentGray15};
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  appearance: none;
   border-radius: 8px;
+`;
+
+const DisplayWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  height: 43px;
+  border-radius: 8px;
+  display: flex;
+  margin-right: auto;
+  padding: 12px 16px 0 16px;
+`;
+
+const IconWrapper = styled.div`
+  padding-left: 20px;
 `;
 
 export default Select;
