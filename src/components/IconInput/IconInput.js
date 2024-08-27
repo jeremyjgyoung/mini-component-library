71c1a17dfff0ca99148e72a7d2cc33d9baebe297
Jeremy <jeremyjgyoung@gmail.com>
2024-08-27 16:21:16 -0700

    refactor: Update IconInput component to use IconComponent and add styling for input and icon

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,14 +9,26 @@ import VisuallyHidden from "../VisuallyHidden";
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   return (
     <InputWrapper>
-      <Icon id={icon}></Icon>
+      <IconComponent id={icon}></IconComponent>
       <Input placeholder={placeholder}></Input>
     </InputWrapper>
   );
 };
 
-const InputWrapper = styled.div``;
+const InputWrapper = styled.div`
+  position: relative;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  padding-left: 32px;
+  pointer-events: none;
+`;
+
+const IconComponent = styled(Icon)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+`;
 
 export default IconInput;
